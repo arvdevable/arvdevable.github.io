f5919d1dbf4204902bd1d975314855f24ea6493b
arvdevable <arvdevlazycode@gmail.com>
2023-03-03 22:27:22 +0700

    Summary (required)

--- script.js
+++
@@ -1,8 +0,0 @@
-const menu = document.getElementById("myMenu");
-
-menu.addEventListener("change", function() {
-  const selectedOption = this.options[this.selectedIndex];
-  window.location = selectedOption.value + ".html";
-});
-
-
